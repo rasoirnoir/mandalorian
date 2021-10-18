8bb642383d22abb646c2715ba0dea29ff75a3cb2
rasoirnoir <w.noris@protonmail.com>
2021-10-18 21:27:45 +0200

    reorg de simages, ajout d'un catch dans api_call()

--- scripts/util.js
+++ scripts/util.js
@@ -3,7 +3,7 @@ console.log("util.js loaded");
 const URL = "https://swapi.dev/api";
 
 /**
- * Envoie un GET à l'api swappi
+ * Envoie une requête GET à l'api swappi
  * @param {string} endpoint l'endpoint à interroger (ex: planets)
  * @param {function} callback La fonction qui traitera la réponse JSON en retour
  */
@@ -14,6 +14,10 @@ export const api_call = (endpoint, callback) => {
         })
         .then(jsonResp => {
             callback(jsonResp);
+        })
+        .catch(error => {
+            console.log(`Erreur lors de l'appel API : 
+                            ${error.message}`);
         });
 }
 
