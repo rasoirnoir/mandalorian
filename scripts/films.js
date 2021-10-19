8d7069003cf5693141da39757eaa5fdf9b09ba49
Blandine Bajard <83599148+BlandineBajard@users.noreply.github.com>
2021-10-19 13:04:58 +0200

    changements CSS pour titre et menu/dans films affichage des titres des films/dans html ajout des class et section pour lien css et js

--- scripts/films.js
+++ scripts/films.js
@@ -1,5 +1,23 @@
-const EPISODE4 = document.getElementById('Ep4');
-const DONNEES = document.getElementById('donnees');
-const VAISSEAUX = document.getElementById('vaisseaux');
-const PLANETES = document.getElementById('planetes');
+const FILMS = document.getElementById('reponses-films');
+const DEPLIE = document.getElementById ('deploiement-films');
 
+console.log(FILMS);
+
+const API_URL = "https://swapi.dev/api/films/";
+fetch(API_URL)
+.then((response) => {
+    console.log(response);
+    return response.json();
+})
+.then(afficherFilms => {
+    console.log(afficherFilms);
+    for(let index = 0; index <6; index++){
+        console.log(afficherFilms.results[index]);
+        const EPISODES = afficherFilms.results[index];
+        console.log(EPISODES);
+    
+        FILMS.innerHTML +=`
+        <p class= "titre">${EPISODES.title}</p>
+    `
+    }
+})
